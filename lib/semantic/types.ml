685dd990936b839c8d683916737cf8e2c7bb4998
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-10-02 17:17:19 +0330

    Added: sizeof and alignof cases

--- lib/semantic/types.ml
+++ lib/semantic/types.ml
@@ -44,3 +44,46 @@ and enumerator = {
   enum_name : string;
   enum_value : int64;
 }
+
+let sizeof = function
+  | Basic Void -> Error "cannot take size of void"
+  | Basic Bool -> Ok 1L
+  | Basic (Char | SignedChar | UnsignedChar) -> Ok 1L
+  | Basic (Short | UnsignedShort) -> Ok 2L
+  | Basic (Int | UnsignedInt) -> Ok 4L
+  | Basic (Long | UnsignedLong) -> Ok 8L
+  | Basic (LongLong | UnsignedLongLong) -> Ok 8L
+  | Basic Float -> Ok 4L
+  | Basic Double -> Ok 8L
+  | Basic LongDouble -> Ok 16L
+  | Pointer _ -> Ok 8L
+  | Array (elem_type, Some count) ->
+      sizeof elem_type |> Result.map (Int64.mul count)
+  | Array (_, None) -> Error "cannot take size of unsized array"
+  | Struct (_, Some fields) -> compute_struct_size fields
+  | Struct (_, None) -> Error "cannot take size of incomplete struct"
+  | Union (_, Some fields) -> compute_union_size fields
+  | Union (_, None) -> Error "cannot take size of incomplete union"
+  | Enum _ -> Ok 4L  (* Enums are int-sized *)
+  | Function _ -> Error "cannot take size of function"
+  | Qualified (t, _) -> sizeof t
+  | Typedef (_, t) -> sizeof t
+
+let alignof = function
+  | Basic Bool -> 1L
+  | Basic (Char | SignedChar | UnsignedChar) -> 1L
+  | Basic (Short | UnsignedShort) -> 2L
+  | Basic (Int | UnsignedInt) -> 4L
+  | Basic (Long | UnsignedLong) -> 8L
+  | Basic (LongLong | UnsignedLongLong) -> 8L
+  | Basic Float -> 4L
+  | Basic Double -> 8L
+  | Basic LongDouble -> 16L
+  | Pointer _ -> 8L
+  | Array (elem_type, _) -> alignof elem_type
+  | Struct (_, Some fields) -> compute_struct_alignment fields
+  | Union (_, Some fields) -> compute_union_alignment fields
+  | Enum _ -> 4L
+  | Qualified (t, _) -> alignof t
+  | Typedef (_, t) -> alignof t
+  | _ -> failwith "alignof: incomplete type"
