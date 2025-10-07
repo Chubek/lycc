c0ee7500af03bf825d6670a5ac7e313edc825bd5
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-10-07 10:12:54 +0330

    Added, Defined: push scope functions for symtbl

--- lib/symtbl.ml
+++ lib/symtbl.ml
@@ -1,24 +1,34 @@
 module Symtbl = struct
   module Identifier = struct
     type t =
-      { uid: sym_id
+      { uid: ident_id
       ; value: string
-      ; kind: sym_kind
+      ; kind: ident_kind
       }
 
-    and sym_kind =
+    and ident_kind =
       | StringConst
       | VariableName
       | FunctionName
       | LabelName
       | TypeAlias
-      | TypeTag
+      | DatatypeTag
+      | FormalParam
+      | ActualParam
+      | EnumConsit
 
-    and sym_id = int
+    and ident_id = int
 
     let compare s1 s2 = Int.compare s1.uid s2.uid
     let hash s = Hashtbl.hash s.uid
     let equal s1 s2 = (=) s1.uid s2.uid
+
+    let uid_counter = ref 0
+
+    let issue_uid () =
+      let uid = !uid_counter in
+      incr uid_counter;
+      uid
   end
 
   module IdentifierMap = Map.Make(Identifier)
@@ -26,19 +36,22 @@ module Symtbl = struct
   type t = scope Stack.t
 
   and scope = 
-    { map: symbol_info IdentifierMap.t
+    { mutable map: symbol_info IdentifierMap.t
     ; level: scope_level
     }
 
   and symbol_info =
-    { stgcls: storage_class
-    ; value: ASTNode.t
+    { stgcls: storage_class option
+    ; typ: TypeLattice.Type.t
+    ; defined: bool
+    ; value: Absyn.ASTNode.t
+    ; loc: Absyn.Location.t
     }
 
   and scope_level =
-    | TransUnit
-    | FunctionLevel
-    | Block
+    | FileScope of string
+    | FunctionBody of Identifier.t
+    | Block of int * Identifier.t
 
   and storage_class =
     | ExternSTG
@@ -46,4 +59,28 @@ module Symtbl = struct
     | AutoSTG
     | RegisterSTG
     | ThreadLocalSTG
+    | TypedefSTG
+
+  let enter_file_scope stb filename =
+    let scope = { map = IdentifierMap.empty ; level = FileScope filename } in
+    Stack.push scope stb;
+    stb
+
+  let enter_function_scope stb funname =
+    let top_scope = Stack.top stb in
+    let scope = { map = top_scope.map ; level = FunctionBody funname } in
+    Stack.push scope stb;
+    stb
+
+  let enter_block_scope stb =
+    let top_scope = Stack.top stb in
+    let (funname, nesting) = 
+      match top_scope.level with
+      | FunctionBody fnnm -> (fnnm, 1)
+      | Block (n, fnm) -> (fnm, n)
+      | _ -> failwith "Block scope must be parent of function scope"
+    in
+    let scope = { map = top_scope.map ; level = BlockScope (nesting, funname) } in
+    Stack.push scope stb;
+    stb
 end
